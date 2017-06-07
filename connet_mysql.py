7bf4eb0a3ee049942bc4bf0a1820eb8d48f7ec76
zack <zackhsw318@outlook.com>
2017-06-07 16:15:15 +0800

    connect db

---
+++ connet_mysql.py
@@ -0,0 +1,28 @@
+import pymysql
+from builtins import int
+
+# 连接数据库
+conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='hDA186', db='hda_drc')
+# 创建游标
+cursor = conn.cursor()
+conn.set_character_set('utf8')
+cursor.execute('SET NAME utf8;')
+cursor.execute('SET CHARACTER SET utf8;')
+cursor.execute('SET character_set_connection=utf8;')
+
+sql = "select * from files_drc"
+sql_desc = "desc files_drc"
+sql_insert = "insert into files_drc values ('asdfas23','中心','aa\/bb\/','333','docx',)"
+
+cursor.execute(sql_insert)
+
+cursor.execute(sql_desc)
+
+print(cursor.execute(sql))
+row = cursor.fetchone()  # 获取一行
+
+print(row)
+conn.commit()
+cursor.close()
+
+conn.close()
