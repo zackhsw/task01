1f92f70c6ceebea995044787cb6e15e7a79eedbb
zack <zackhsw318@outlook.com>
2017-06-08 13:43:27 +0800

    测试连接 提交

---
+++ get_user_ip.py
@@ -0,0 +1,17 @@
+import socket
+
+
+def get_ip():
+    result = []
+    # 获取本机电脑名
+    user_name = socket.getfqdn(socket.gethostname())
+    result.append(user_name)
+    # 获取本机ip 这是内网的ip !!!
+    user_ip = socket.gethostbyname(user_name)
+    # result[1] = user_ip
+    result.append(user_ip)
+    return result
+
+#  阿什顿发斯蒂芬
+for item in get_ip():
+    print(item)
